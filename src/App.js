81c01a25249a9ff37ce1f7701cb89b8defd1c6e8
Roberto Mauro Perez Olivera <robertom.perez@outlook.com>
2023-04-21 21:40:18 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import React from 'react'
+import { Appi } from './components/Appi';
+
+
+
+const App = () => {
+  return (
+     
+      
+       <Appi />
+
+  )
+}
+
+export default App
+      
+  
+
+
+
